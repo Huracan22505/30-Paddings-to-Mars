f37d9157b0c4d5f008221b4ab368c8c0944e9756
Aleksandr Rafa <huracan22505@protonmail.com>
2020-09-18 00:10:10 +0300

    Updates

--- menu.234110a1.js
+++ menu.234110a1.js
@@ -140,6 +140,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   function toggleModal() {
     document.body.classList.toggle('burger-menu-open');
     refs.menu.classList.toggle('is-open');
+    document.body.classList.toggle('menu-open');
   }
 })();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -170,7 +171,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59598" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61333" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
