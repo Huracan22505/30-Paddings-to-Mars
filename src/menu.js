32be4a1de198944ecacc00c7c347420019bda8d2
Marina Vyshnevska <Marinessa393@users.noreply.github.com>
2020-09-17 15:11:26 +0300

    fix

--- src/menu.js
+++ src/menu.js
@@ -7,6 +7,7 @@
     menuLink2: document.querySelector('[link2]'),
     menuLink3: document.querySelector('[link3]'),
     menuLink4: document.querySelector('[link4]'),
+    menuLink5: document.querySelector('[link5]'),
   };
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
@@ -14,6 +15,7 @@
   refs.menuLink2.addEventListener('click', toggleModal);
   refs.menuLink3.addEventListener('click', toggleModal);
   refs.menuLink4.addEventListener('click', toggleModal);
+  refs.menuLink5.addEventListener('click', toggleModal);
   function toggleModal() {
     document.body.classList.toggle('burger-menu-open');
     refs.menu.classList.toggle('is-open');
